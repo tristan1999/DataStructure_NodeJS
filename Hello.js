b8d0bf33ae4f8a1f21a4ee1d80f2c337349a83db
tristan1999 <80997776+tristan1999@users.noreply.github.com>
2021-09-29 14:11:38 +0800

    0929

--- Hello.js
+++ Hello.js
@@ -2,20 +2,29 @@
 //JS PPT: https://drive.google.com/open?id=0BwN5VWV_C7-5ZnE3d045YWZJb00
 //Javascript 變數的使用
 
-var weight=50.5;
-var height=160;
+const readline = require('readline-sync');
+weight = readline.question('請輸入您的體重(kg)?');
+height = readline.question('請輸入您的身高(cm)?');  
 var bmi = weight/((height/100)**2);
 
-console.log("Hello! Your BMI value is "+ bmi);
 
+if (height > 220 || height < 50){
+    console.Log(身高不合理)
+    } else
+    console.log('身高合理')
+    ;
+if (weight > 200 || weight < 10){
+    console.log(體重不合理)
+} else
+      console.log('體重合理')
+    ;
+console.log("Hello! Your BMI value is "+ bmi);
 //使用第三方套件 readline-sync (待使用者輸入資料後程式再繼續執行) 
 //NodeJS 套件管理程式為 npm，需打開終端機輸入指令
 //安裝 readline-sync 套件指令: npm install readline-sync
 //另可透過package.json指定所有相關套件後 npm install 即可安裝所有套件
 
-const readline = require('readline-sync');
-weight = readline.question('請輸入您的體重(kg)?');
-height = readline.question('請輸入您的身高(cm)?');  
+
 
 
 //not only bmi value, but also judgement
